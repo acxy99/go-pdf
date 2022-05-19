d958d0b587e6ace5c29cda7f1ee8edec056c1dca
acxy179 <alicechin@misoft.io>
2022-05-19 12:02:25 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module go-pdf
+
+go 1.18
+
+require github.com/adrg/go-wkhtmltopdf v0.3.0
+
+require (
+	github.com/SebastiaanKlippert/go-wkhtmltopdf v1.7.2 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	u "go-pdf/pdfGenerator"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	r := u.NewRequestPdf("")
+
+	//html template path
+	templatePath := "templates/sample.html"
+
+	//path for download pdf
+	outputPath := "storage/exampl2.pdf"
+
+	//html template data
+	templateData := struct {
+		Title       string
+		Description string
+		Company     string
+		Contact     string
+		Country     string
+	}{
+		Title:       "HTML to PDF generator",
+		Description: "This is the simple HTML to PDF file.",
+		Company:     "Jhon Lewis",
+		Contact:     "Maria Anders",
+		Country:     "Germany",
+	}
+
+	if err := r.ParseTemplate(templatePath, templateData); err == nil {
+		err := r.GeneratePDF(outputPath)
+		if err != nil {
+			fmt.Println("unable to print pdf")
+		}
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func exportHandler(w http.ResponseWriter, r *http.Request) {
+
+	// filebytes, err := ioutil.ReadFile("storage/example.pdf")
+	// if err != nil {
+	// 	fmt.Println("unable to read file")
+	// }
+	// if err := r.ParseForm(); err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// path := r.FormValue("path")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	basepath, _ := os.Getwd()
+
+	fileLocation := filepath.Join(basepath, "/storage/example.pdf")
+
+	Openfile, err := os.Open(fileLocation) //Open the file to be downloaded later
+
+	if err != nil {
+		http.Error(w, "File not found.", 404) //return 404 if file is not found
+		return
+	}
+	defer Openfile.Close() //Close after function return
+
+	// tempBuffer := make([]byte, 512) //Create a byte array to read the file later
+	// Openfile.Read(tempBuffer)
+
+	cd := fmt.Sprintf("attachment; filename=%s", "WHATEVER_YOU_WANT.pdf")
+	w.Header().Set("Content-Disposition", cd)
+
+	if _, err := io.Copy(w, Openfile); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
